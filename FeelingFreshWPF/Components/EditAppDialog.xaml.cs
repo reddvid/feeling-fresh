62b66cbafd01f6d5b5b2290da69ef2cd1d014f9d
David Ballesteros <david_ballesteros@outlook.com>
2023-01-02 22:36:18 +0800

    transform app for exe only

---
+++ FeelingFreshWPF/Components/EditAppDialog.xaml.cs
@@ -0,0 +1,68 @@
+﻿using FeelingFreshWPF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Components
+{
+	/// <summary>
+	/// Interaction logic for EditAppDialog.xaml
+	/// </summary>
+	public partial class EditAppDialog : UserControl
+	{
+		private string originalName;
+
+		public EditAppDialog()
+		{
+			InitializeComponent();
+
+			btnUpdate.IsEnabled = true;
+		}
+
+		public EditAppDialog(string appName) : this()
+		{
+			if (string.IsNullOrWhiteSpace(appName)) return;
+
+			FillDetails(appName);
+		}
+
+		private void FillDetails(string appName)
+		{
+			originalName = appName;
+			tbxAppName.Text = appName;
+
+			btnUpdate.IsEnabled = false;
+		}
+
+		private void tbxAppName_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(tbxAppName.Text) || tbxAppName.Text.ToLower() == originalName.ToLower()) return;
+
+			// Enable Update button
+			btnUpdate.IsEnabled = true;
+		}
+
+		private void CloseDialog_Click(object sender, RoutedEventArgs e)
+		{
+			Window.GetWindow(this).Close();
+		}
+
+
+		private async void UpdateApp_Click(object sender, RoutedEventArgs e)
+		{
+			await DBHelper.UpdateApp(originalName, tbxAppName.Text);
+
+			Window.GetWindow(this).Close();
+		}
+	}
+}
