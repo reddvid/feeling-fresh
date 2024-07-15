d7a112c03995c10e0332afd2fc7941374cba02fa
David Ballesteros <david_ballesteros@outlook.com>
2024-07-15 20:17:44 +0800

    Add back up minecraft

--- BeFresh.WPF/MainWindow.xaml.cs
+++ BeFresh.WPF/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 using Path = System.IO.Path;
 
 namespace BeFresh.WPF;
@@ -31,7 +33,10 @@ public partial class MainWindow : Window
                                     "\\Packages\\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\\LocalState";
 
     private readonly string _minecraft =
-        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft";
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\";
+
+    private readonly string? _temp = Environment.GetEnvironmentVariable("Temp");
+
 
     private const string FolderName = "BeFreshBackups";
 
@@ -52,7 +57,7 @@ public partial class MainWindow : Window
         }
     }
 
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
     {
         var tag = (sender as Button)?.Tag as string;
 
@@ -64,50 +69,14 @@ public partial class MainWindow : Window
         }
         else if (tag.Equals("minecraft"))
         {
-            BackupMinecraftFolder();
+            await BackupMinecraftFolder();
         }
     }
-
-    private void BackupMinecraftFolder()
-    {
-        // Scan folder
-        string path = MC_SAVES_FOLDER + selectedSaveName;
-        if (Directory.Exists(path.Trim()))
-        {
-            string fileNameInitial = string.Empty;
-
-            // If OneDrive is not found, save in Documents folder
-            if (!String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("OneDrive")))
-            {
-                fileNameInitial =
-                    $"{ONEDRIVE_FOLDER}\\{selectedSaveName}_{Environment.MachineName}_{DateTime.Now:MMddyy}.zip";
-            }
-            else if (!String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("OneDriveConsumer")))
-            {
-                fileNameInitial =
-                    $"{ONEDRIVECONSUMER_FOLDER}\\{selectedSaveName}_{Environment.MachineName}_{DateTime.Now:MMddyy}.zip";
-            }
-            else
-            {
-                fileNameInitial =
-                    $"{DOCS_FOLDER}\\{selectedSaveName}_{Environment.MachineName}_{DateTime.Now:MMddyy}.zip";
-            }
-
-            backupFileName = fileNameInitial;
-            int count = 1;
-
-            while (File.Exists(backupFileName))
-            {
-                backupFileName = GenerateFileName(fileNameInitial, count++);
-            }
-
-            await ScanFiles(backupFileName);
-        }
-    }
-
+    
     private void BackupStartMenuLayout()
     {
-        var path = Path.Combine(_oneDrivePersonal, FolderName) + "\\" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        var path = Path.Combine(_oneDrivePersonal, FolderName) + "\\StartMenu\\" +
+                   DateTime.Now.ToString("yyyyMMdd_HHmmss");
         if (!Directory.Exists(path))
         {
             Directory.CreateDirectory(path);
@@ -154,61 +123,95 @@ public partial class MainWindow : Window
         }
     }
 
+    private async Task BackupMinecraftFolder()
+    {
+        btnMinecraftBackup.IsEnabled = false;
+        
+        var path = Path.Combine(_oneDrivePersonal, FolderName) + "\\Minecraft\\" +
+                   DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        Console.WriteLine(_minecraft);
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        try
+        {
+            if (Directory.Exists(path.Trim()))
+            {
+                var fileNameInitial = $"{path}\\minecraft.zip";
+
+                var backupFileName = fileNameInitial;
+                int count = 1;
+
+                while (File.Exists(backupFileName))
+                {
+                    backupFileName = GenerateFileName(fileNameInitial, count++);
+                }
+
+                Console.WriteLine(backupFileName);
+                await ScanFiles(backupFileName);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            btnMinecraftBackup.IsEnabled = true;
+        }
+    }
+
     private async Task ScanFiles(string filename)
     {
-        tempFileName = $"{TEMP_FOLDER}\\" + filename.Substring(filename.IndexOf(selectedSaveName.Trim() + "_"),
-            filename.Length - filename.IndexOf(selectedSaveName.Trim() + "_"));
-
+        var tempFileName = $"{_temp}\\minecraft.zip";
         var fsOut = File.Create(tempFileName);
-        pBar.IsIndeterminate = isBusy = true;
+        var zipStream = new ZipOutputStream(fsOut);
+        zipStream.SetLevel(7); //0-9, 9 being the highest level of compression
+        int folderOffset = (_minecraft).Length + ((_minecraft).EndsWith("\\") ? 0 : 1);
 
-        zipStream = new ZipOutputStream(fsOut);
-        zipStream.SetLevel(5); //0-9, 9 being the highest level of compression
-        int folderOffset = (MC_SAVES_FOLDER + selectedSaveName).Length +
-                           ((MC_SAVES_FOLDER + selectedSaveName).EndsWith("\\") ? 0 : 1);
-
-        await Task.Run(() => CompressFolder((MC_SAVES_FOLDER + selectedSaveName), zipStream, folderOffset));
+        await Task.Run(() => CompressFolder(_minecraft, zipStream, folderOffset));
 
         zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
         zipStream?.Close();
 
-        pBar.IsIndeterminate = isBusy = false;
-
         if (File.Exists(tempFileName)) File.Move(tempFileName, filename);
-
-        CompleteDialog.Show();
-        //MessageBox.Show("Export complete.");
-
-        BackupBtn.Content = "Start Backup";
     }
 
-    private void CompressFolder(string DATA_FOLDER, ZipOutputStream zipStream, int folderOffset)
+    private void CompressFolder(string dataFolder, ZipOutputStream zipStream, int folderOffset)
     {
-        string[] files = Directory.GetFiles(DATA_FOLDER, ".", SearchOption.AllDirectories);
+        Console.WriteLine(dataFolder);
+        string[] files = Directory.GetFiles(dataFolder, ".", SearchOption.AllDirectories);
 
         foreach (string filename in files)
         {
+            if (filename.Contains(@".minecraft\backups\") || filename.Contains(@".minecraft\assets\")) continue;
+            
+            Console.WriteLine(filename);
+            
             try
             {
                 FileInfo fi = new FileInfo(filename);
 
                 string entryName =
-                    @"\" + selectedSaveName.Trim() + "\\" +
-                    filename.Substring(folderOffset); // Makes the name in zip based on the folder
+                    string.Concat("\\", filename.AsSpan(folderOffset)); // Makes the name in zip based on the folder
                 entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
-                ZipEntry newEntry = new ZipEntry(entryName);
-                newEntry.DateTime = fi.LastWriteTime; // Note the zip format stores 2 second granularity
+                var newEntry = new ZipEntry(entryName)
+                {
+                    DateTime = fi.LastWriteTime, // Note the zip format stores 2 second granularity
+                    // Specifying the AESKeySize triggers AES encryption. Allowable values are 0 (off), 128 or 256.
+                    // A password on the ZipOutputStream is required if using AES.
+                    //   newEntry.AESKeySize = 256;
+                    // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003, WinZip 8, Java, and other older code,
+                    // you need to do one of the following: Specify UseZip64.Off, or set the Size.
+                    // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, you do not need either,
+                    // but the zip will be in Zip64 format which not all utilities can understand.
+                    //   zipStream.UseZip64 = UseZip64.Off;
+                    Size = fi.Length
+                };
 
-                // Specifying the AESKeySize triggers AES encryption. Allowable values are 0 (off), 128 or 256.
-                // A password on the ZipOutputStream is required if using AES.
-                //   newEntry.AESKeySize = 256;
-
-                // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003, WinZip 8, Java, and other older code,
-                // you need to do one of the following: Specify UseZip64.Off, or set the Size.
-                // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, you do not need either,
-                // but the zip will be in Zip64 format which not all utilities can understand.
-                //   zipStream.UseZip64 = UseZip64.Off;
-                newEntry.Size = fi.Length;
                 zipStream.PutNextEntry(newEntry);
                 // Zip the file in buffered chunks
                 // the "using" will close the stream even if an exception occurs
@@ -224,6 +227,7 @@ public partial class MainWindow : Window
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
+                throw;
             }
         }
     }
@@ -239,7 +243,5 @@ public partial class MainWindow : Window
 
     private async void Backup_Click(object sender, RoutedEventArgs e)
     {
-       
-           
     }
 }
